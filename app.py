97b30acd10d8ea90745cc5a8ddce33b405c75733
Srija Talukdar <52798333+Srija17@users.noreply.github.com>
2021-06-09 20:37:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import joblib 
+model=joblib.load('sentiment_analyzer')
+st.title('Sentiment Analyzer')
+ip = st.text_input("Enter the Review")
+op = model.predict([ip])
+if st.button('Predict'):
+  st.title(op[0])
